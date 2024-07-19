ea31e0500a17b9ff11b9bc97f051660340ad4735
secona <77039267+secona@users.noreply.github.com>
2024-07-19 18:07:07 +0700

    feat: move to compiler + vm

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-use belalang_core::{lexer::Lexer, parser::Parser};
-use belalang_eval::{
-    builtins::{self, Builtins},
-    evaluator::Evaluator,
-    object::Object,
-};
+use belalang_comp::compiler::CompilerBuilder;
+use belalang_core::lexer::Lexer;
+use belalang_core::parser::Parser;
+use belalang_vm::builtins::{Builtin, BuiltinCollectionBuilder};
+use belalang_vm::object::Object;
+use belalang_vm::vm::VMBuilder;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -11,43 +11,44 @@ extern "C" {
     fn println(value: &str);
 }
 
-#[wasm_bindgen(start)]
-pub fn start() {
-    let mut fns = builtins::BUILTIN_FUNCTIONS.lock().unwrap();
-
-    fns.insert(
-        "println".into(),
-        Box::new(|args| {
-            println(
-                &args
-                    .iter()
-                    .map(|arg| arg.to_string())
-                    .collect::<Vec<_>>()
-                    .join(" "),
-            );
-
-            Object::Null
-        }),
-    );
-}
-
 #[wasm_bindgen]
 pub fn run_code(input: String) {
     let lexer = Lexer::new(input.as_bytes());
     let mut parser = Parser::new(lexer);
 
-    match parser.parse_program() {
-        Ok(program) => {
-            let builtins = Builtins::default();
-            let mut ev = Evaluator::new(builtins);
-
-            if let Err(err) = ev.eval_program(program) {
-                println(&err.to_string());
-            }
-        }
+    let program = match parser.parse_program() {
+        Ok(program) => program,
         Err(err) => {
-            println(&err.to_string());
+            println(&format!("Syntax Error: {}", err));
             return;
         }
+    };
+
+    let mut compiler = CompilerBuilder::default().build();
+    let mut code = match compiler.compile_program(program) {
+        Ok(code) => code,
+        Err(err) => {
+            println(&format!("Compile Error: {}", err));
+            return;
+        }
+    };
+
+    let bc = BuiltinCollectionBuilder::default()
+        .builtin_function(
+            "print".into(),
+            Builtin {
+                arity: 1,
+                function: Box::new(|args| {
+                    println(&format!("{}", args.first().unwrap()));
+                    Object::Null
+                }),
+            },
+        )
+        .build();
+
+    let mut vm = VMBuilder::default().builtin_collection(bc).build();
+    vm.append_code(&mut code);
+    if let Err(err) = vm.run() {
+        println(&format!("Runtime Error: {}", err));
     }
 }
